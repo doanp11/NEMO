b6521c508b5056257c5a9a59e86587b2327d68a1
doanp11 <doanp@lnf.umich.edu>
2021-07-12 16:28:08 -0400

    - fixed bug on validating only transactions with end dates before 5 days prior to now

--- NEMO/apps/NEMO_transaction_validation/management/commands/auto_validate_transactions.py
+++ NEMO/apps/NEMO_transaction_validation/management/commands/auto_validate_transactions.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'Auto validates Usage Events with end dates that have finished 5 days from now'
 
     def handle(self, *args, **options):
-        outdated_transactions = UsageEvent.objects.filter(validated=False, end__lte=timezone.now() + timedelta(days=5))
+        outdated_transactions = UsageEvent.objects.filter(validated=False, end__lte=timezone.now() - timedelta(days=5))
         for ot in outdated_transactions:
             if not Contest.objects.filter(admin_approved=False, transaction=ot.id).exists():
                 ot.validated = True
